102ccbbdfdb2bca6706248e9f3f98f84a4ca2a8f
yingbing.zhao <1121391968@qq.com>
2019-07-25 18:37:00 +0800

    change table-test

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-
+import Table from "./views/Table.vue";
 Vue.use(Router);
 
 export default new Router({
@@ -19,6 +19,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+        path: "/table",
+        name: "table",
+        component: Table
     }
   ]
 });
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -4,6 +4,18 @@
       :data="tableData"
       style="width: 100%">
       <el-table-column
+              type="selection"
+              width="55">
+      </el-table-column>
+      <el-table-column  label="" align="center" width="150" show-overflow-tooltip>
+         <template slot-scope="scope">
+            <el-checkbox v-model="scope.row.checked" @change="handleItem"></el-checkbox>
+         </template>
+         <template slot="header" slot-scope="slot">
+            <el-checkbox v-model="checkall" :indeterminate="isIndeterminate" @change="handleChange"></el-checkbox>
+         </template>
+      </el-table-column>
+      <el-table-column
         prop="date"
         label="日期"
         width="180">
@@ -30,21 +42,28 @@ export default {
        tableData: [{
             date: '2016-05-02',
             name: '王小1',
-            address: '上海市普陀区金沙江路 1518 弄'
+            address: '上海市普陀区金沙江路 1518 弄',
+            checked:false
           }, {
             date: '2016-05-04',
             name: '王小2',
-            address: '上海市普陀区金沙江路 1517 弄'
+            address: '上海市普陀区金沙江路 1517 弄',
+           checked:false
           }, {
             date: '2016-05-01',
             name: '王小3',
-            address: '上海市普陀区金沙江路 1519 弄'
+            address: '上海市普陀区金沙江路 1519 弄',
+           checked:false
           }, {
             date: '2016-05-03',
             name: '王小4',
-            address: '上海市普陀区金沙江路 1516 弄'
+            address: '上海市普陀区金沙江路 1516 弄',
+            checked:false
           }],
-          change:''
+          change:'',
+          multipleSelection:[],
+          checkall:false,
+          isIndeterminate: false
     }
   },
   methods:{
@@ -53,17 +72,44 @@ export default {
       const _this = this
       Sortable.create(tbody, {
         onEnd({ newIndex, oldIndex }) {
-          const currRow = _this.tableData.splice(oldIndex, 1)[0]
-          _this.tableData.splice(newIndex, 0, currRow)
+            const currRow = _this.tableData .splice(oldIndex, 1)[0]
+            _this.tableData .splice(newIndex, 0, currRow)
+            let newArray = _this.tableData.slice(0)
+            _this.tableData= []
+            _this.$nextTick(() => {
+                _this.tableData = newArray
+            })
         },
         onUpdate(){
             _this.change='primary';
         }
       })
+    },
+    handleChange(val){
+         if(val){
+             this.tableData.forEach(item=>{
+                 item.checked=true;
+             })
+         }else{
+             this.tableData.forEach(item=>{
+                 item.checked=false;
+             })
+         }
+    },
+    handleItem(){
+        let flag=this.tableData.every(item=>{
+            return item.checked==true;
+       })
+        if(flag){
+            this.isIndeterminate=false;
+        }else{
+            this.isIndeterminate=true;
+            this.checkall=true;
+        }
     }
   },
   mounted(){
-    this.rowDrop();
+   this.rowDrop();
   }
 };
 </script>
