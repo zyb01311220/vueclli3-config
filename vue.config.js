18486b47e90f7e6a6727bf56478e30701ace443c
yingbing.zhao <1121391968@qq.com>
2019-08-08 16:02:23 +0800

    修改webpack配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 module.exports = {
     publicPath: './',
     outputDir: 'dist',
@@ -9,11 +11,52 @@ module.exports = {
     },
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-            return {
-                plugins: [
-                    new BundleAnalyzerPlugin()
-                ]
-            }
+            // let optimization = {
+            //     splitChunks: {
+            //         chunks: 'all',//在做代码分割时,只对异步代码生效,写成all的话,同步异步代码都会分割
+            //         minSize: 20000, // 依赖包超过20kb才做代码分割
+            //         maxAsyncRequests: 5,//同时加载的模块最多是5个
+            //         maxInitialRequests: 3, //入口文件做代码分割最多分成3个js
+            //         name: true,//让cacheGroups里面的名字生效
+            //         automaticNameDelimiter: '~',//文件生成时的连接符
+            //         cacheGroups: {//当打包同步代码时,上面的参数生效
+            //             vendors: {
+            //                 test: /[\\/]node_modules[\\/]/,//检测引入的库是否在node_modules里
+            //                 priority: -10,//值越大,优先级越高,莫乱先打包到优先级高的组里
+            //                 filename:'vendors.js'//所有的库都打包到vendor.js这个文件里
+            //             },
+            //             default: {
+            //                 minChunks: 2,//当一个包至少被用了多少次才进行代码分割
+            //                 priority: -20,
+            //                 reuseExistingChunk: true,//如果一个模块被打包过了,再次打包就会忽略
+            //                 filename:'common.js'
+            //             }
+            //         }
+            //     }
+            // }
+            // Object.assign(config, {
+            //     optimization
+            // })
+            const plugins = [];
+            plugins.push(
+                new CompressionWebpackPlugin({
+                    filename: '[path].gz[query]',
+                    algorithm: 'gzip',
+                    test: productionGzipExtensions,
+                    threshold: 10240,
+                    minRatio: 0.8
+                }),
+                new BundleAnalyzerPlugin()
+            );
+            config.plugins = [
+                ...config.plugins,
+                ...plugins
+            ];
+            // return {
+            //     plugins: [
+            //         new BundleAnalyzerPlugin()
+            //     ]
+            // }
         }
     },
     css: {
